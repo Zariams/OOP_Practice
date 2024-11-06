ef316f5952578f1fc794ca80d5c1be166bf03115
Zariams <m.s.zaria@student.khai.edu>
2024-11-06 16:00:56 +0200

    Added test methods for Tenant class

--- TestProject/Tenant.cs
+++ TestProject/Tenant.cs
@@ -3,15 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OOP_Practice;
 
 namespace TestProject
 {
-    public class Tenant
+    [TestClass]
+    public class TestTenant
     {
 
         [TestMethod]
-        public void TestMethod1()
+        public void ConstructorTest_correct()
         {
+            //Arrange
+            string name = "John Smith";
+            DateTime birthdate = new DateTime(1980, 1, 1);
+            //Act
+            Tenant tenant = new Tenant(name, birthdate);
+            //Assert
+            Assert.AreEqual(name, tenant.Name);
+            Assert.AreEqual(birthdate, tenant.BirthDate);
+            Assert.IsNotNull(tenant.Account);
+        }
+        [TestMethod]
+        public void ConstructorTest_incorrect_Name()
+        {
+            //Arrange
+            string name1 = "12345";
+            string name2 = "A";
+            string name3 = "_____";
+            string name4 = "";
+            DateTime birthdate = new DateTime(1980, 1, 1);
+            //Act + Assert
+            Assert.ThrowsException<ArgumentException>(() => new Tenant(name1, birthdate));
+            Assert.ThrowsException<ArgumentException>(() => new Tenant(name2, birthdate));
+            Assert.ThrowsException<ArgumentException>(() => new Tenant(name3, birthdate));
+            Assert.ThrowsException<ArgumentException>(() => new Tenant(name4, birthdate));
+        }
+        [TestMethod]
+        public void ConstructorTest_incorrect_Date()
+        {
+            //Arrange
+            string name = "John Smith";
+            DateTime birthdate1 = new DateTime(2980, 1, 1);
+            DateTime birthdate2 = DateTime.Now;
+            DateTime birthdate3 = DateTime.Now.AddYears(-200);
+            //Act + Assert
+            Assert.ThrowsException<ArgumentException>(() => new Tenant(name, birthdate1));
+            Assert.ThrowsException<ArgumentException>(() => new Tenant(name, birthdate2));
+            Assert.ThrowsException<ArgumentException>(() => new Tenant(name, birthdate3));
         }
     }
 }
